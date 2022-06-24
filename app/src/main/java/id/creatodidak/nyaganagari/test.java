9aa239953155eaf5622065363dd8279579965aee
Your Name <you@example.com>
2022-06-25 02:08:24 +0700

    test update

---
+++ app/src/main/java/id/creatodidak/nyaganagari/test.java
@@ -0,0 +1,14 @@
+package id.creatodidak.nyaganagari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
